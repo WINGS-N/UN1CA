4cb1480ba8eee9daaff26f97dc2bb191666a88f1
Nikita <72886646+WINGS-N@users.noreply.github.com>
2026-08-11 02:43:29 +0000

    [target] pick the teegris applet set when building

--- target/b0s/patches/vendor/customize.sh
+++ target/b0s/patches/vendor/customize.sh
@@ -1,3 +1,9 @@
+TEE_SET="tee_eyb1"
+
+LOG "- Installing TEEgris applets from $TEE_SET"
+EVAL "rm -rf \"$WORK_DIR/vendor/tee\"" || exit 1
+EVAL "cp -a \"$MODPATH/$TEE_SET\" \"$WORK_DIR/vendor/tee\"" || exit 1
+
 if ! grep -q "init_31_0 tee_file" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
     {
         echo "(allow init_31_0 tee_file (dir (mounton)))"
